6bf7ff77eaa8512b14b6d69a9774aa92d761a72d
AdelaYolis <230712240@students.uajy.ac.id>
2025-12-03 13:08:35 +0700

    edit add to cart

--- cart/add.php
+++ cart/add.php
@@ -6,5 +6,41 @@ if(!isset($_POST['product_id']) || !isset($_POST['size'])) {
     die("Invalid request");
 }
 
-$product_id = intval
+$product_id = intval($_POST['product_id']);
+$size = $_POST['size'];
+$qty = isset($_POST['qty']) ? intval($_POST['qty']) : 1;
+
+//ambil data produk
+$stmt = $mysqli->prepare("SELECT id, name, price FROM product WHERE id = ?");
+$stmt->bind_param("i", $product_id);
+$stmt->execute();
+$product = $stmt->get_result()->fetch_assoc();
+
+if(!$product) {
+    die("Product not found");
+}
+
+//Jika cart belum ada, buat baru
+if(!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+}
+
+//productID + size
+$key = $product_id . "_" . $size;
+
+//Jika sudah ada, tambahkan qty
+if(isset($_SESSION['cart'][$key])) {
+    $_SESSION['cart'][$key]['qty'] += $qty;
+}else{
+    $SESSION['cart'][$key] = [
+        "id" => $product['id'],
+        "name" => $product['name'],
+        "size" => $size,
+        "qty" => $qty,
+        "price" => $product['price']
+    ];
+}
+
+header("Location: listCart.php");
+exit;
 ?>
