f3c3d12a020a47fc4942331b0bb9e01099c5a124
AdelaYolis <230712240@students.uajy.ac.id>
2025-12-09 09:01:10 +0700

    edit cart.css

--- cart/listCart.php
+++ cart/listCart.php
@@ -139,8 +139,8 @@ $cart = $resultCart->fetch_all(MYSQLI_ASSOC);
                         <button class="select-btn">Select</button>
                     </form>
 
-                  <a href="cart/delete.php?id=<?= (int)$item['cart_id'] ?>" class="delete-btn">
-                    Delete
+                    <a href="cart/delete.php?id=<?= (int)$item['cart_id'] ?>" class="delete-btn">
+                        Delete
                     </a>
                 </div>
 
@@ -207,8 +207,5 @@ $cart = $resultCart->fetch_all(MYSQLI_ASSOC);
 
 </div> 
 </main>
-
-<a href="product/listProduct.php" class="back-link">← Back to Products</a>
-
 </body>
 </html>
